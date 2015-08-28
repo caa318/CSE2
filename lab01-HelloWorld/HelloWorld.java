96b473ddcadf5b4b823381814539deb7f6774005
caa318 <caa318@lehigh.edu>
2015-08-28 15:48:12 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+//
+//Chris Alexander
+//CSE2 Lab 01
+//Hello Wolrd Java
+//
+//first compile the Java Program
+//      javac HelloWorld.java
+//then run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld{
+
+//add main method
+  public static void main(String[] args){
+        
+//print out the statement with a box
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("##                   ####");
+    System.out.println("##   Hello World!!   ####");   
+    System.out.println("##                   ####");
+    System.out.println("#########################");     
+    System.out.println("#########################");
+    System.out.println("test");
+    
+    //test/testtest
+  }
+    }
