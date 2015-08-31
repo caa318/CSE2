2c31e18ffff61c2cba9ce8a35d45a6a1804bdb11
caa318 <caa318@lehigh.edu>
2015-08-31 00:13:13 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//
+//Chris Alexander
+//CSE2 HW 01
+//Welcome Class
+//
+//first compile the Java Program
+//      javac WelcomeClass.java
+//then run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+
+//add main method
+  public static void main(String[] args){
+        
+//print out the statement with a box
+    System.out.println("  -----------");     
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");  
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-C--A--A--3--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I am the president of the Men's Volleyball Team here at Lehigh.");
+    
+    //test/testtest
+  }
+    }
